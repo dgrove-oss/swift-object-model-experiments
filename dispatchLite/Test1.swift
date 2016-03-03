7dfff96a77a3ef85baed03521a843a7ac9672c5b
David Grove <groved@us.ibm.com>
2016-03-03 15:54:51 +0000

    simplify test; add rule to generate .sil file

--- dispatchLite/Test1.swift
+++ dispatchLite/Test1.swift
@@ -2,10 +2,9 @@ import DispatchLite;
 
 func test() -> Void {
   let q = dispatch_make_queue(10,20);
-  for _ in 1...5 {
-     let o = dispatch_increase_data(q, 40);
-     print("Data was \(o)");
-  }
+  let o = dispatch_increase_data(q, 40);
+  let o2 = dispatch_increase_data(q, o);
+  let o3 = dispatch_increase_data(q, o2);
 }
 
 test();
